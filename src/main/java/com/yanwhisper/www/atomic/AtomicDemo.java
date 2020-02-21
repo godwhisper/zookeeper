6e00c161a564087a0ec1fa26dcfb074e3cc57520
whisper <18826137377@sina.cn>
2020-02-21 15:24:41 +0800

    原子类test

--- src/main/java/com/yanwhisper/www/atomic/AtomicDemo.java
+++ src/main/java/com/yanwhisper/www/atomic/AtomicDemo.java
@@ -13,8 +13,12 @@ public class AtomicDemo {
     public static void main(String args[]) {
         // 与lock的区别：获取锁失败不会被挂起，而是会重新尝试获取锁，直到成功为止
         // 存在的问题：ABA问题（追加版本号）；循环时间长开销大；只能保证一个共享变量的原子操作（可以把多个变量放在一个对象里进行CAS操作）
-        AtomicInteger atomicInteger = new AtomicInteger();
+        AtomicInteger atomicInteger = new AtomicInteger(1);
+        atomicInteger.compareAndSet(1, 5);
+        System.out.println(atomicInteger.get());
         // 附带版本号的原子类
         AtomicStampedReference<String> atomicStampedReference = new AtomicStampedReference<>("a", 1);
+        atomicStampedReference.compareAndSet("a", "b", 1, 3);
+        System.out.println(atomicStampedReference.getReference() + "-" + atomicStampedReference.getStamp());
     }
 }
